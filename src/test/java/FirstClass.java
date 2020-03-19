a146ca8331ce716ac4011fb27f97e6d69ec7fe08
Fieruza <61644117+Fieruza@users.noreply.github.com>
2020-03-18 20:17:57 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello world ");
         System.out.println("Feruz");
+        System.out.println("Hakuna Matata");
     }
 }
